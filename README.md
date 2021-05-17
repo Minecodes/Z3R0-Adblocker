bd61009ddf70e0a7c3a120263dcc203a256f5699
Minecodes <6444823-Minecodes13@users.noreply.gitlab.com>
2021-05-17 15:13:22 +0200

    Creating addon

---
+++ README.md
@@ -0,0 +1,6 @@
+# Z3R0 Adblocker
+Another adblocker...<br/><br/>
+This adblocker is in development and is opensource.<br/>
+That means, you can help here too and you can add more site to the list.<br/>
+The versions are for Chrome, Firefox, Brave, Edge, Opera and Vivaldi.<br/>
+All is tested on Brave, Firefox, Chrome and Chromium. (Chromium Version: 88.0.4305.0 (sorry, but i don't use this browser often))
---
+++ inject.js
@@ -0,0 +1,35 @@
+/**
+ * header-ad-link
+ * ad
+ * ad-dfp
+ * ad-detect
+ * ad-label
+ * ad-label-wrapper
+ * responsive-skyscraper
+ * sidebar
+ */
+
+for (let i = 0; i < document.getElementsByClassName("header-ad-link").length; i++) {
+    document.getElementsByClassName("header-ad-link").item(i).remove();
+}
+for (let i = 0; i < document.getElementsByClassName("ad").length; i++) {
+    document.getElementsByClassName("ad").item(i).remove();
+}
+for (let i = 0; i < document.getElementsByClassName("ad-dfp").length; i++) {
+    document.getElementsByClassName("ad-dfp").item(i).remove();
+}
+for (let i = 0; i < document.getElementsByClassName("ad-detect").length; i++) {
+    document.getElementsByClassName("ad-detect").item(i).remove();
+}
+for (let i = 0; i < document.getElementsByClassName("ad-label").length; i++) {
+    document.getElementsByClassName("ad-label").item(i).remove();
+}
+for (let i = 0; i < document.getElementsByClassName("ad-label-wrapper").length; i++) {
+    document.getElementsByClassName("ad-label-wrapper").item(i).remove();
+}
+for (let i = 0; i < document.getElementsByClassName("responsive-skyscraper").length; i++) {
+    document.getElementsByClassName("responsive-skyscraper").item(i).remove();
+}
+for (let i = 0; i < document.getElementsByClassName("sidebar").length; i++) {
+    document.getElementsByClassName("sidebar").item(i).remove();
+}
